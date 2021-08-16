b5f9e8f6384d2ca21bb6a4a9d299f4419e937ed0
OliverMurphy <35534522+OliverMurphy@users.noreply.github.com>
2021-08-16 11:05:12 -0400

    added comments for dependencies

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+//imported dependencies using npm
+//npm install @material-ui/core
+//npm install @material-ui/icons
+
 ReactDOM.render(
   <App />,
   document.getElementById("root")
